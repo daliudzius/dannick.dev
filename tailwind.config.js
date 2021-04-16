13dc0a997b3dda7251458d5f77e7166321ab1d78
daliudzius <daliudzius@gmail.com>
2021-04-15 18:27:33 -0700

    Added initial structure. May need reformat.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,49 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+   mode: 'jit',
+   purge: {
+      content: [
+         './pages/**/*.{js,ts,jsx,tsx}',
+         './components/**/*.{js,ts,jsx,tsx}',
+      ],
+      options: {
+         keyframes: true,
+      },
+   },
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+      extend: {
+         colors: {
+            bluegray: colors.blueGray,
+         },
+         animation: {
+            pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+            'pulse-subtle':
+               'pulse-subtle 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+         },
+         keyframes: {
+            pulse: {
+               '0%, 100%': {
+                  opacity: 0.5,
+               },
+               '50%': {
+                  opacity: 1,
+               },
+            },
+            'pulse-subtle': {
+               '0%, 100%': {
+                  opacity: 0.7,
+               },
+               '50%': {
+                  opacity: 1,
+               },
+            },
+         },
+      },
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
 }
