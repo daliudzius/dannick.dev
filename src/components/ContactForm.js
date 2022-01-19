98ab2ca3faef2cdade626448b9d5faf891f68499
daliudzius <daliudzius@gmail.com>
2022-01-19 03:33:24 -0800

    update styling on contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,7 @@ export default function ContactForm() {
    const [message, setMessage] = useState('')
 
    const onSubmitSuccess = () => {
-      setValidationMessage('Message successfully sent!')
+      setValidationMessage('Successfully Sent!')
       setCanSubmit(true)
    }
 
@@ -23,7 +23,6 @@ export default function ContactForm() {
       }
 
       data.email = data.email.toLowerCase()
-      console.log(data)
       const response = await fetch('/api/contact', {
          method: 'POST',
          headers: { 'Content-Type': 'multipart/form-data' },
@@ -31,57 +30,62 @@ export default function ContactForm() {
       })
 
       await response.json()
-      onSubmitSuccess()
+      if (response.status === 200) {
+         onSubmitSuccess()
+      }
    }
 
    return (
       <div>
-         {validationMessage && (
-            <span
-               className={
-                  'block text-base md:text-xl bg-green-200 dark:text-green-700 px-3 py-2 rounded-md mb-10 text-center'
-               }
-            >
-               {validationMessage}
-            </span>
-         )}
-
          <form onSubmit={handleSubmit}>
-            <div className='flex flex-col my-8'>
-               <label className='mb-1'>Name</label>
+            <div className='flex flex-col mt-6 mb-8'>
+               <label className='mb-1 text-gray-400'>name</label>
                <input
                   required
                   type='text'
                   name='name'
                   onChange={(e) => setName(e.target.value)}
+                  className='pl-2 rounded dark:bg-gray-800 ring-1 ring-purple-300 dark:ring-transparent h-9'
                />
             </div>
 
             <div className='flex flex-col mb-8'>
-               <label className='mb-1'>Email</label>
+               <label className='mb-1 text-gray-400'>email</label>
                <input
                   required
                   type='email'
                   name='email'
                   onChange={(e) => setEmail(e.target.value)}
+                  className='pl-2 rounded dark:bg-gray-800 ring-1 ring-purple-300 dark:ring-transparent h-9'
                />
             </div>
 
             <div className='flex flex-col mb-8'>
-               <label className='mb-1'>Message:</label>
+               <label className='mb-1 text-gray-400'>message</label>
                <textarea
                   required
                   name='message'
                   rows={4}
                   type='text'
                   onChange={(e) => setMessage(e.target.value)}
+                  className='pt-2 pl-2 rounded dark:bg-gray-800 ring-1 ring-purple-300 dark:ring-transparent'
                />
             </div>
 
-            <div className='flex flex-col items-start mb-4'>
-               <button type='submit' className='' disabled={!canSubmit}>
-                  Send
-               </button>
+            <div className='flex items-start mb-4'>
+               {validationMessage ? (
+                  <span className='flex items-center justify-center px-3 text-lg bg-green-300 rounded-md dark:gradient-purple-pink h-9'>
+                     {validationMessage}
+                  </span>
+               ) : (
+                  <button
+                     type='submit'
+                     className='w-20 text-lg text-purple-600 transition rounded outline-none ring-inset h-9 dark:text-white ring-2 dark:hover:ring-0 dark:focus:ring-white ring-purple-400 dark:ring-purple-500 dark:hover:gradient-purple-pink dark:focus:gradient-purple-pink focus:bg-purple-500 hover:bg-purple-500 focus:ring-purple-500 hover:ring-purple-500 hover:text-white focus:text-white'
+                     disabled={!canSubmit}
+                  >
+                     Send
+                  </button>
+               )}
             </div>
          </form>
       </div>
